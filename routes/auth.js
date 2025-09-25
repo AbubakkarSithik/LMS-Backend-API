cc6b77d565292c769c2b8d2bce076626eaaefff6
AbubakkarSithik <abusithik482@gmail.com>
2025-09-25 15:54:10 +0530

    Added email redirection url + CORS

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
-  const { data, error } = await supabase.auth.signUp({ email, password });
+  const { data, error } = await supabase.auth.signUp({ email, password, options: { emailRedirectTo: process.env === "development" ? "http://localhost:5173/onboard-redirect" :  `${window.location.origin}/onboard-redirect`} });
   if (error) return res.status(400).json({ error: error.message });
 
   res.json({ user: data.user, session: data.session });
