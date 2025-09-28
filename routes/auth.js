450efee35aeda0b7527aad807b161ffe721983de
AbubakkarSithik <abusithik482@gmail.com>
2025-09-28 12:56:58 +0530

    Added new user info collect api's

--- routes/auth.js
+++ routes/auth.js
@@ -60,18 +60,65 @@ router.post("/login", async (req, res) => {
   res.json({ user: data.user });
 });
 
+//set-session
 router.post("/set-session", async (req, res) => {
   const { access_token, refresh_token } = req.body;
   if (!access_token || !refresh_token) {
     return res.status(400).json({ error: "Missing tokens" });
   }
 
-  // Validate access token
-  const { data, error } = await supabase.auth.getUser(access_token);
-  if (error) return res.status(401).json({ error: error.message });
+  const { data: userData, error } = await supabase.auth.getUser(access_token);
+  if (error || !userData?.user) {
+    return res.status(401).json({ error: error?.message || "Invalid access token" });
+  }
 
-  setAuthCookies(res, { access_token, refresh_token });
-  res.json({ user: data.user ,session: data.session });
+  const session = {
+    access_token,
+    refresh_token,
+    token_type: "bearer",
+    expires_in: 60 * 60, // 1 hour
+    user: userData.user,
+  };
+
+  setAuthCookies(res, session);
+
+  res.json({ user: userData.user, session });
+});
+
+
+router.post("/set-password", async (req, res) => {
+  const {  new_password } = req.body;
+  const sb_access_token = req.cookies.sb_access_token;
+  const sb_refresh_token = req.cookies.sb_refresh_token;
+  if (!sb_access_token || !sb_refresh_token || !new_password) {
+    return res.status(400).json({ error: "Missing access_token, refresh_token or new_password" });
+  }
+
+  // update password
+  const { data: updateData, error: updateError } = await supabase.auth.updateUser(
+    { password: new_password },
+    { sb_access_token }
+  );
+
+  if (updateError) {
+    return res.status(400).json({ error: updateError.message });
+  }
+
+  const { data: sessionData, error: refreshError } = await supabase.auth.refreshSession({
+    refresh_token: sb_refresh_token,
+  });
+
+  if (refreshError || !sessionData.session) {
+    return res.status(401).json({ error: refreshError?.message || "Failed to refresh session" });
+  }
+
+  setAuthCookies(res, sessionData.session);
+
+  res.json({
+    message: "Password updated and user logged in successfully",
+    user: sessionData.user,
+    session: sessionData.session,
+  });
 });
 
 // Restore session from refresh token
